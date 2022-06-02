099ad46734e8cf2320f79a35b5c6f27671d33d5b
Demis Russo <demis.silva@compasso.com.br>
2022-06-02 11:48:11 -0300

    feat: Home page
    Add styled home page with a SubscribeButton component

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import styles from './styles.module.scss'
+
+export function SubscribeButton() {
+  return (
+    <button type='button' className={styles.subscribeButton}>
+      Subscribe now
+    </button>
+  )
+}
