9eed337314fdedd1e2994677b08e46120ba926dc
Demis Russo <demis.silva@compasso.com.br>
2022-06-10 15:34:18 -0300

    feat: prismic
    Add a new service prismic.ts with a initial configuration

---
+++ src/services/prismic.ts
@@ -0,0 +1,23 @@
+import * as prismic from '@prismicio/client'
+import { enableAutoPreviews } from '@prismicio/next'
+
+export const endpoint = process.env.PRISMIC_ENDPOINT
+export const repositoryName = prismic.getRepositoryName(endpoint)
+
+// This factory function allows smooth preview setup
+export function createClient(config: any = {}) {
+  console.log({ endpoint, repositoryName })
+
+  const client: prismic.Client = prismic.createClient(endpoint, {
+    ...config,
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN
+  })
+
+  enableAutoPreviews({
+    client,
+    previewData: config.previewData,
+    req: config.req
+  })
+
+  return client
+}
